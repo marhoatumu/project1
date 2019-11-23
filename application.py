a59dff678ca8efa35a4a7f0f66092e2da1d841e9
marhoatumu <52777437+marhoatumu@users.noreply.github.com>
2019-11-23 15:37:33 +0100

    Created static folder and moved all static files there to better work with Flask. Also made necessary changes to application.py to suit this

--- application.py
+++ application.py
@@ -1,11 +1,11 @@
 import os
 
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, url_for
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
