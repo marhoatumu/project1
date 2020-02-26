f38f8daf448689ef6c4744f3611f9f99afb72579
marhoatumu <52777437+marhoatumu@users.noreply.github.com>
2020-02-26 16:52:27 +0100

    Update import.py

--- import.py
+++ import.py
@@ -4,7 +4,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine(os.getenv('postgres://iyzxosnvirakvx:aa8462466b57b6dd9a0a55a90eef19b45873614d0f3c19b99c4a5a21b8a0edf4@ec2-107-22-228-141.compute-1.amazonaws.com:5432/d7eo12mqipiu42'))
+engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 def main():
@@ -14,6 +14,9 @@ def main():
         db.execute("INSERT INTO books (title, author, year, isbn) VALUES (:title, :author, :year, :isbn)",
         {"title": title, "author": author, "year": year, "isbn": isbn})
        # sumTotal = db.execute ("COUNT(*) FROM books")
-       # print(f"Added "sumTotal")
+        print(f"Added {title} by {author}.")
 
-        db.commit()
+    db.commit()
+
+    if __name__ == "__main__":
+            main()
